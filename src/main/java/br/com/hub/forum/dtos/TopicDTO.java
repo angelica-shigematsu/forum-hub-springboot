51b75c932d917f217a5bb0e4933ee5e0a250dc0e
Angélica <angelica_shigematsu@outlook.com>
2024-06-19 13:26:14 -0300

    feat: add list topic dto

--- src/main/java/br/com/hub/forum/dtos/TopicDTO.java
+++ src/main/java/br/com/hub/forum/dtos/TopicDTO.java
@@ -1,33 +1,20 @@
 package br.com.hub.forum.dtos;
 
 import br.com.hub.forum.models.StatusTopic;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import org.springframework.data.annotation.CreatedDate;
-
-import java.time.LocalDate;
 
 public record TopicDTO(
         @NotBlank
         String title,
         @NotBlank
         String message,
-        @CreatedDate
-        LocalDate dateCreated,
-
         @NotNull
         StatusTopic statusTopic,
 
         @NotNull
-        @Valid
-        UserDTO author,
+        long idAuthor,
 
         @NotNull
-        @Valid
-        CourseDTO course,
-
-        @NotNull
-        @Valid
-        ResponseDTO response) {
+        long idCourse) {
 }
